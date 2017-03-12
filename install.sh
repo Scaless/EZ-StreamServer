9048c60c20466cf9abaf2a97f0748056d79cf1cf
Scaless <Scaless@users.noreply.github.com>
2017-03-11 23:32:26 -0500

    Remove mysql/php dependencies

--- install.sh
+++ install.sh
@@ -2,27 +2,18 @@
 
 #Packages needed to install everything
 EZSTREAM_NEEDED_PACKAGES="build-essential libpcre3 libpcre3-dev libssl-dev wget git"
-EZSTREAM_OPTIONAL_PACKAGES="mysql-server php-fpm php-mysql"
+EZSTREAM_OPTIONAL_PACKAGES=""
 #Root paths for building files
 EZSTREAM_ROOT="$HOME"
 EZSTREAM_BUILD_PATH="$EZSTREAM_ROOT/EZstreambuild"
 EZSTREAM_SOURCE_PATH="$HOME/EZ-StreamServer-master"
 #Download locations
 NGINX_TAR_PATH="https://nginx.org/download/nginx-1.11.10.tar.gz"
-#NGINX_TAR_NAME=$(echo $NGINX_TAR_PATH | sed "s/.*\///")
 NGINX_RTMP_TAR_PATH="https://github.com/arut/nginx-rtmp-module/archive/v1.1.11.tar.gz"
-#NGINX_RTMP_TAR_NAME=$(echo $NGINX_RTMP_TAR_PATH | sed "s/.*\///")
-
-MARIADB_PASSWORD="EZStreamDB"
 
 #Begin installation
 echo "Installing required packages"
 apt-get -q update && apt-get -q install $EZSTREAM_NEEDED_PACKAGES -y
-
-export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MARIADB_PASSWORD"
-sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MARIADB_PASSWORD"
-apt-get -q install $EZSTREAM_OPTIONAL_PACKAGES -y
 
 mkdir $EZSTREAM_BUILD_PATH
 cd $EZSTREAM_BUILD_PATH
@@ -48,7 +39,6 @@ make install >>"$EZSTREAM_BUILD_PATH/nginx_install.log" 2>&1
 #Copy Default Files
 cp "$EZSTREAM_SOURCE_PATH/defaultfiles/index.html" "/usr/local/nginx/html/"
 cp "$EZSTREAM_SOURCE_PATH/defaultfiles/nginx.conf" "/usr/local/nginx/conf/"
-cp "$EZSTREAM_SOURCE_PATH/defaultfiles/info.php" "/usr/local/nginx/html/"
 
 #start nginx
 /usr/local/nginx/sbin/nginx
